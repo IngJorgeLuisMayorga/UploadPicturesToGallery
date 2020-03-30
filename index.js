ad7377171df8f1e2c71bd44845082ed8a5fffe84
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2020-03-29 23:03:44 -0500

    Fix

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const getImprintsBySku = require('./lib/imprints');
 const downloadFile = require('./lib/download');
 const compressFile = require('./lib/compress');
 const uploadFile = require('./lib/upload');
-const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
@@ -18,90 +17,37 @@ const fs = require('fs');
     'catalog/santé-et-bien-être', 
     'catalog'];
 
-    const categories = CATEGORIES_AWS_JLMAYORGACO;
-
-
-    const INIT_TIME = 60000; // 60 sec
-    const CATEGORY_BASE = 'http://localhost:4201/';
-
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
-
-    console.log(' Initing ...., waiting chrome getting ready ')
-    await wait(INIT_TIME);
-    console.log(' Ready ')
-
-
-    for (let c = 0; c < categories.length ; c++) {
-        
         // For Each Category
-        const category = categories[c];
-        await page.goto( `${CATEGORY_BASE}${category}`);
-        await wait(30000);
+        const category = 'hults';
         console.log(' ');
         console.log('CATEGORY => '+category);
         console.log(' ');
 
-        const nOfItemsQuery = `JSON.stringify(Math.max(...Array.from(document.querySelectorAll('span a')).map(i => parseInt(i.innerText, 10)).filter(i => !isNaN(i))))`;
-        const noOfItemsAsString = await page.evaluate(nOfItemsQuery);
-        const noOfString = JSON.parse(noOfItemsAsString);
-        const pagesLength = parseInt(noOfString, 10);
-        await wait(3500);
-        console.log(' ');
-        console.log('PAGES => '+pagesLength);
-        console.log(' '); 
+        
 
-    
-        for (let p = 0; p < pagesLength + 1 ; p++) {
+        // skus  for HULTS
+        const skus = ["8713159256571","8713159101093","8713159100393","8713159103448","8713159128373","8713159109853","8713159198697","8713159214946","8713159122302","8713159163343","8713159249085","8713159124863","8713159165460","8713159259121","8713159332954","8713159369189","8713159364115","8713159192954","8713159237228","8713159365051","8713159238478","8713159308645","8713159170297","8713159142218","8713159165811","8713159101093","8713159100393","8713159103448","8713159128373","8713159109853","8713159198697","8713159214946","8713159122302","8713159163343","8713159249085","8713159124863","8713159165460","8713159259121","8713159332954","8713159369189","8713159364115","8713159192954","8713159237228","8713159365051","8713159238478","8713159308645","8713159170297","8713159142218","8713159165811","8713159101093","8713159100393","8713159103448","8713159128373","8713159109853","8713159198697","8713159214946","8713159122302","8713159163343","8713159249085","8713159124863","8713159165460","8713159259121","8713159332954","8713159369189","8713159364115","8713159192954","8713159237228","8713159365051","8713159238478","8713159308645","8713159170297","8713159142218","8713159165811","8713159101093","8713159100393","8713159103448","8713159128373","8713159109853","8713159198697","8713159214946","8713159122302","8713159163343","8713159249085","8713159124863","8713159165460","8713159259121","8713159332954","8713159369189","8713159364115","8713159192954","8713159237228","8713159365051","8713159238478","8713159308645","8713159170297","8713159142218","8713159165811","8713159101093","8713159100393","8713159103448","8713159128373","8713159109853","8713159198697","8713159214946","8713159122302","8713159163343","8713159249085","8713159124863","8713159165460","8713159259121","8713159332954","8713159369189","8713159364115","8713159192954","8713159237228","8713159365051","8713159238478","8713159308645","8713159170297","8713159142218","8713159165811"]
 
-            // For Each Page
-            const skus = [];
-            const queryAsString = `JSON.stringify(Array.from(document.getElementsByClassName("${'catalog-product-item'}")).map(el => el.id))`;
-            const skusAsString = await page.evaluate(queryAsString);
-            const skusAsArray = JSON.parse(skusAsString);
-            skusAsArray.map(sku => skus.push(sku));
-            await wait(3500); 
-            console.log(' ');
-            console.log('SKUS => '+JSON.stringify(skus));
-            console.log(' ');
+        // For Each Pages SKUS
+        const N = skus.length;
+        for(let s = 0; s < N ; s++){
+            const sku = skus[s];
+            const imprints = await getImprintsBySku(sku);
+            
+            for(let i = 0; i < imprints.length ; i++){
+            
+                try{
+                    const filename = await downloadFile(imprints[i].image);
+                    const compress = await compressFile(filename);
+                    const upload = await uploadFile(filename);
 
+                } catch( error ){
 
-            //click next
-            try{
-                await page.$eval('.pagination-next', el => el.click());
-            } catch( errorClickNext ){
-                await page.screenshot({path: 'error.png'});
-            }
-
-            // For Each Pages SKUS
-            const N = skus.length;
-            for(let s = 0; s < N ; s++){
-                const sku = skus[s];
-                const imprints = await getImprintsBySku(sku);
-                
-                for(let i = 0; i < imprints.length ; i++){
-               
-                    try{
-                        const filename = await downloadFile(imprints[i].image);
-                        const compress = await compressFile(filename);
-                        const upload = await uploadFile(filename);
-                        console.log({filename})
-                        process.exit(0)
-
-                    } catch( error ){
-
-                    }
-
-                    console.log(`CATEGORY ${category} PAGE ${p} SKU ${sku} Image ${imprints[i].image}`);
                 }
+
+                console.log(`CATEGORY ${category} SKU ${sku} Image ${imprints[i].image}  i ${i}/${imprints.length}`);
             }
-
-
-
         }
 
-    }
-
-    await browser.close();
 
 })();
